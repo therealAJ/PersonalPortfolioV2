e77ac88e01ef77586f07b89552e86f2401ecabe3
Alex <alexonej@gmail.com>
2016-07-15 19:58:43 -0700

    put in sliders + redid nav bar .. needs tweaking for responsive screens

--- js/main.js
+++ js/main.js
@@ -5,24 +5,125 @@ $(".button-collapse").sideNav();
 
 $(document).ready(function () {
 
-    $("#toSlide").hide();
-
-
-    $('#home-toggle').click(function () {
-        $("#about-me").hide();
-        $('.button-collapse').sideNav('hide');
-        $("#toSlide").slideToggle();
-
-    });
-
-    $("#about-me").hide();
-
-
-    $('#about-toggle').click(function () {
-        $("#toSlide").hide();
-        $('.button-collapse').sideNav('hide');
-        $("#about-me").slideToggle();
-    });
-
+    $("#about-me-container").hide();
+    $("#experience-container").hide();
+    $("#projects-container").hide();
+    $("#skills-container").hide();
+    $("#interests-container").hide();
+    $("#contact-container").hide();
 
 });
+
+function onHomeClick() {
+
+    setTimeout(function () {
+        $("#headline").fadeIn();
+    }, 500);
+
+
+    $("#experience-container").slideUp();
+    $("#projects-container").slideUp();
+    $("#skills-container").slideUp();
+    $("#interests-container").slideUp();
+    $("#contact-container").slideUp();
+    $("#about-me-container").slideUp();
+
+}
+
+
+function onAboutClick() {
+
+    $("#headline").fadeOut(500);
+
+    $("#experience-container").slideUp();
+    $("#projects-container").slideUp();
+    $("#skills-container").slideUp();
+    $("#interests-container").slideUp();
+    $("#contact-container").slideUp();
+
+    setTimeout(function () {
+        $("#about-me-container").slideDown();
+    }, 500);
+
+
+}
+
+function onExperienceClick() {
+
+    $("#headline").fadeOut(500);
+
+    $("#about-me-container").slideUp();
+    $("#projects-container").slideUp();
+    $("#skills-container").slideUp();
+    $("#interests-container").slideUp();
+    $("#contact-container").slideUp();
+
+    setTimeout(function () {
+        $("#experience-container").slideDown();
+    }, 500);
+
+}
+
+function onProjectsClick() {
+
+    $("#headline").fadeOut(500);
+
+    $("#about-me-container").slideUp();
+    $("#experience-container").slideUp();
+    $("#skills-container").slideUp();
+    $("#interests-container").slideUp();
+    $("#contact-container").slideUp();
+
+    setTimeout(function () {
+        $("#projects-container").slideDown();
+    }, 500);
+
+
+
+}
+
+function onSkillsClick() {
+
+    $("#headline").fadeOut(500);
+
+    $("#experience-container").slideUp();
+    $("#projects-container").slideUp();
+    $("#about-me-container").slideUp();
+    $("#interests-container").slideUp();
+    $("#contact-container").slideUp();
+
+    setTimeout(function () {
+        $("#skills-container").slideDown();
+    }, 500);
+
+}
+
+function onInterestsClick() {
+
+    $("#headline").fadeOut(500);
+
+    $("#experience-container").slideUp();
+    $("#projects-container").slideUp();
+    $("#skills-container").slideUp();
+    $("#about-me-container").slideUp();
+    $("#contact-container").slideUp();
+
+    setTimeout(function () {
+        $("#interests-container").slideDown();
+    }, 500);
+}
+
+function onContactClick() {
+
+    $("#headline").fadeOut(500);
+
+    $("#about-me-container").slideUp();
+    $("#experience-container").slideUp();
+    $("#projects-container").slideUp();
+    $("#skills-container").slideUp();
+    $("#interests-container").slideUp();
+
+    setTimeout(function () {
+        $("#contact-container").slideDown();
+    }, 500);
+}
