5d6728fc149da601fda6b23792156b6c6e5a8a24
Alex <alexonej@gmail.com>
2016-07-18 19:50:06 -0700

    add modals

--- js/main.js
+++ js/main.js
@@ -2,6 +2,24 @@
 $(".button-collapse").sideNav();
 
 
+//
+//MODAL
+$(document).ready(function () {
+    $('.modal-trigger').leanModal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: 0.5, // Opacity of modal background
+        in_duration: 300, // Transition in duration
+        out_duration: 200, // Transition out duration
+        //      ready: function() { alert('Ready'); }, // Callback for Modal open
+        //      complete: function() { alert('Closed'); } // Callback for Modal close
+    })
+});
+
+//$(document).ready(function () {
+//    $('.modal-trigger').leanModal();
+//});
+
+
 
 $(document).ready(function () {
 
